3297779bf11f5945c4d667aa3dc2e83003e823b4
Cameron DeCoster <cameron.decoster@gmail.com>
2019-12-26 12:44:35 -0700

    cleaned up layouts

--- main.js
+++ main.js
@@ -1,26 +1,33 @@
 const header = document.getElementById("header");
-//header.style.height = `${window.innerHeight}px`;
-let count = 0;
-//console.log(header);
-//console.log(screen.height)
+const windowWidth = document.documentElement.clientWidth;
+/*console.log(windowWidth)
+
+if (windowWidth < 900) {
+    header.style.height = `${window.innerHeight}px`;
+    let count = 0;    
+}
 
 window.onscroll = function (event) {
-    //console.log(origHeight);
-    const windowHeight = window.innerHeight;
-    console.log(`Window height is ${windowHeight}`);
-    const calcHeight = windowHeight - window.pageYOffset;
-    console.log(`Calc height is ${calcHeight}`);
-    console.log(window.scrollY);
-    console.log(window.pageYOffset);
-    if (window.pageYOffset && count < 10) {
-        if (calcHeight < 300) {
-            //header.style.height = '300px';
-        }
-        else {
-            //console.log(window.pageYOffset);
-            //header.style.height = `${calcHeight}px`;
-            console.log('Header height is ' + header.style.height);
-            count++;
-        }
+    const windowWidth = document.documentElement.clientWidth;
+    if (windowWidth < 900) {
+        //console.log(origHeight);
+        const windowHeight = window.innerHeight;
+        console.log(`Window height is ${windowHeight}`);
+        const calcHeight = windowHeight - window.pageYOffset;
+        console.log(`Calc height is ${calcHeight}`);
+        console.log(window.scrollY);
+        console.log(window.pageYOffset);
+        if (window.pageYOffset) {
+            if (calcHeight < 108) {
+                header.style.height = '108px';
+            }
+            else {
+                //console.log(window.pageYOffset);
+                header.style.height = `${calcHeight}px`;
+                console.log('Header height is ' + header.style.height);
+                
+            }
+        } 
     }
-}
+    
+}*/
