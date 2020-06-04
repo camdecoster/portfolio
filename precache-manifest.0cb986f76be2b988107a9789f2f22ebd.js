beb1e951e389afc5b49c6bfb34f118d75fca431e
Cameron DeCoster <cameron.decoster@gmail.com>
2020-06-04 11:58:57 -0600

    Updates

---
+++ precache-manifest.0cb986f76be2b988107a9789f2f22ebd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aef2a0d356ff7842651dc507a27d886f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5951cfde0a2182eba1c3",
+    "url": "/portfolio/static/css/main.e7223c54.chunk.css"
+  },
+  {
+    "revision": "160d11a3806cedb8bf34",
+    "url": "/portfolio/static/js/2.3503c246.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.3503c246.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5951cfde0a2182eba1c3",
+    "url": "/portfolio/static/js/main.6212c3af.chunk.js"
+  },
+  {
+    "revision": "ef91ffc74e285db8830a",
+    "url": "/portfolio/static/js/runtime-main.5b355ddd.js"
+  },
+  {
+    "revision": "a1f6bc9382af72bf7edf688417634a3b",
+    "url": "/portfolio/static/media/dashboard_categories.a1f6bc93.png"
+  },
+  {
+    "revision": "231bf9a8856eba004f685775a1f34898",
+    "url": "/portfolio/static/media/denver-skyline.231bf9a8.png"
+  },
+  {
+    "revision": "f40a18a4560cc3fb05902f172e3bd83b",
+    "url": "/portfolio/static/media/dtt_frontpage_large.f40a18a4.png"
+  },
+  {
+    "revision": "28d2abd802e95dfab30081ef63b2919e",
+    "url": "/portfolio/static/media/futurama_frontpage_large.28d2abd8.png"
+  }
+]);
